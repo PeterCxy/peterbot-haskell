94398435c2a8f909c2dce21d1da12562ffc90ee5
Peter Cai <peter@typeblog.net>
2017-10-03 19:59:37 +0800

    Commands: avoid the case-of-hell

--- src/Commands.hs
+++ src/Commands.hs
@@ -4,6 +4,7 @@ module Commands where
 
 import Control.Concurrent.Async
 import qualified Data.Text as T
+import Data.UUID
 import Types
 import EventBus
 import Utils
@@ -22,19 +23,22 @@ registerCommands config bus = do
     -- List of commands and their corresponding functions
     cmds = [
       ("hello", cmdHello)]
+    subscriber :: (String, Command) -> EventBus TgUpdate -> TgUpdate -> Maybe (IO ())
+    subscriber pair bus ev = do
+      msg <- message ev
+      txt <- text msg
+      let args = T.splitOn " " $ T.strip txt
+      if (length args /= 0) && (head args == T.concat ["/", T.pack (fst pair)])
+        then return $ snd pair config bus msg args
+        else return $ return () -- Remeber this is Maybe (IO ()), so we need two `return`s
+
     reg :: (String, Command) -> IO ()
     reg pair = do
-      _ <- subscribe bus $ \_ b ev ->
-        case message ev of
-          Nothing -> return ()
-          Just msg -> case text msg of
-            Nothing -> return ()
-            Just txt -> do
-              let args = T.splitOn " " $ T.strip txt
-              --print args
-              if (length args /= 0) && (head args == T.concat ["/", T.pack (fst pair)])
-                then snd pair config b msg args
-                else return ()
+      _ <- subscribe bus $ \_ b ev -> do
+        -- Defer the work to another function wrapped with Maybe monad
+        -- Otherwise we will have to use bunches of case-of
+        let m = subscriber pair b ev
+        defVal m $ return () -- Force unwrap the Maybe monad for evaluation
       return ()
 
 cmdHello :: Command
